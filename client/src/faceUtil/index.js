f244c67b4a57f46d3431930bbe3b06a3b08acb5f
ritikam29 <mahapatraritika2002@gmail.com>
2022-05-29 18:13:53 +0400

    update

--- client/src/faceUtil/index.js
+++ client/src/faceUtil/index.js
@@ -1,77 +1,77 @@
 import * as faceapi from 'face-api.js';
 
 export async function loadModels(
-  setLoadingMessage,
-  setLoadingMessageError
+    setLoadingMessage,
+    setLoadingMessageError
 ) {
-  const MODEL_URL = process.env.PUBLIC_URL + '/models';
+    const MODEL_URL = process.env.PUBLIC_URL + '/models';
 
-  try {
-    setLoadingMessage('Loading Face Detector');
-    await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
+    try {
+        setLoadingMessage('Loading Face Detector');
+        await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
 
-    setLoadingMessage('Loading 68 Facial Landmark Detector');
-    await faceapi.loadFaceLandmarkTinyModel(MODEL_URL);
+        setLoadingMessage('Loading 68 Facial Landmark Detector');
+        await faceapi.loadFaceLandmarkTinyModel(MODEL_URL);
 
-    setLoadingMessage('Loading Feature Extractor');
-    await faceapi.loadFaceRecognitionModel(MODEL_URL);
-  } catch (err) {
-    setLoadingMessageError(
-      'Model loading failed. Please contact me about the bug:attendlytical@gmail.com'
-    );
-  }
+        setLoadingMessage('Loading Feature Extractor');
+        await faceapi.loadFaceRecognitionModel(MODEL_URL);
+    } catch (err) {
+        setLoadingMessageError(
+            'Model loading failed. Please contact me about the bug:attendlytical@gmail.com'
+        );
+    }
 }
 
 export async function getFullFaceDescription(blob, inputSize = 512) {
-  // tiny_face_detector options
-  let scoreThreshold = 0.8;
-  const OPTION = new faceapi.SsdMobilenetv1Options({
-    inputSize,
-    scoreThreshold,
-  });
-  const useTinyModel = true;
+    // we need to set a score threshold to determine between the positive and negative class. The higher the score threshold for face detection, the more strict for the detection with less result and vice versa.
+    let scoreThreshold = 0.8;
+    const OPTION = new faceapi.SsdMobilenetv1Options({
+        inputSize,
+        scoreThreshold,
+    });
+    const useTinyModel = true;
 
-  // fetch image to api
-  let img = await faceapi.fetchImage(blob);
+    // fetch image to api
+    let img = await faceapi.fetchImage(blob);
 
-  // detect all faces and generate full description from image
-  // including landmark and descriptor of each face
-  let fullDesc = await faceapi
-    .detectAllFaces(img, OPTION)
-    .withFaceLandmarks(useTinyModel)
-    .withFaceDescriptors();
-  return fullDesc;
+    // detect all faces and generate full description from image
+    // including landmark and descriptor of each face
+    let fullDesc = await faceapi
+        .detectAllFaces(img, OPTION)
+        .withFaceLandmarks(useTinyModel)
+        .withFaceDescriptors();
+    return fullDesc;
 }
 
 export async function createMatcher(faceProfile, maxDescriptorDistance) {
-  // Create labeled descriptors of member from profile
-  let labeledDescriptors = faceProfile.map(
-    (profile) =>
-      new faceapi.LabeledFaceDescriptors(
-        profile.student._id,
-        profile.facePhotos.map(
-          (photo) => new Float32Array(photo.faceDescriptor.match(/-?\d+(?:\.\d+)?/g).map(Number))
+    // Create labeled descriptors of member from profile
+    let labeledDescriptors = faceProfile.map(
+        (profile) =>
+        new faceapi.LabeledFaceDescriptors(
+            profile.student._id,
+            profile.facePhotos.map(
+                (photo) => new Float32Array(photo.faceDescriptor.match(/-?\d+(?:\.\d+)?/g).map(Number))
+            )
         )
-      )
-  );
+    );
 
-  // Create face matcher (maximum descriptor distance is 0.5)
-  let faceMatcher = new faceapi.FaceMatcher(
-    labeledDescriptors,
-    maxDescriptorDistance
-  );
+    // Create face matcher (maximum descriptor distance is 0.5)
+    let faceMatcher = new faceapi.FaceMatcher(
+        labeledDescriptors,
+        maxDescriptorDistance
+    );
 
-  return faceMatcher;
+    return faceMatcher;
 }
 
 export function isFaceDetectionModelLoaded() {
-  return !!faceapi.nets.ssdMobilenetv1.params;
+    return !!faceapi.nets.ssdMobilenetv1.params;
 }
 
 export function isFeatureExtractionModelLoaded() {
-  return !!faceapi.nets.faceRecognitionNet.params;
+    return !!faceapi.nets.faceRecognitionNet.params;
 }
 
 export function isFacialLandmarkDetectionModelLoaded() {
-  return !!faceapi.nets.faceLandmark68TinyNet.params;
+    return !!faceapi.nets.faceLandmark68TinyNet.params;
 }
